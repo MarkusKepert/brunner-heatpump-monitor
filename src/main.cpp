00437b86a6bde726d0ac096905a24ea414da7ecc
Markus Kepert <mk@dieitexperten.de>
2022-12-27 23:03:12 +0100

    initial commit of project with platformio template and nodeMCU 8266 wifi hardware

---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+#include <Arduino.h>
+#include "ESP8266WiFi.h"
+#include <ESP8266Ping.h>
+
+const char* remote_host = "fritz.box";
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2, OUTPUT);
+
+  Serial.begin(9600);
+  Serial.println();
+
+  WiFi.begin("***REMOVED***", "***REMOVED***");
+
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+
+  delay(100);
+}
+
+void loop() {
+    if(Ping.ping(remote_host)) {
+      Serial.println("Host Online");
+      delay(1000);
+      //digitalWrite(2, HIGH);
+      //digitalWrite(2, LOW);
+    } else {
+    Serial.println("Offline");
+    delay(1000);
+    //digitalWrite(2, LOW);
+    //digitalWrite(2, HIGH);
+    }
+}
